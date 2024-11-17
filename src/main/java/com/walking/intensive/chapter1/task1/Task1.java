ae944931b9428219e5449b35ec37c8b8750ffdde
Oleg <kovalyovop@yandex.ru>
2024-11-18 00:04:26 +0300

    task1 18/11/2024

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -14,15 +14,40 @@ package com.walking.intensive.chapter1.task1;
  */
 public class Task1 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
+
+        int age = -1;
+        int age1 = 0;
+        int age2 = 4;
+        int age3 = 5;
+        int age4 = 14;
+        int age5 = 11;
 
         System.out.println(getAgeString(age));
+        System.out.println(getAgeString(age1));
+        System.out.println(getAgeString(age2));
+        System.out.println(getAgeString(age3));
+        System.out.println(getAgeString(age4));
+        System.out.println(getAgeString(age5));
+
     }
 
     static String getAgeString(int age) {
-//        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+        if (age < 0) {
+            return "Некорректный ввод";
+        }
+
+        int firstNumber = age % 10;
+        int secondNumber = age % 100;
+
+        if (firstNumber == 1 && secondNumber != 11) {
+            return "Вам " + age + " год";
+        }
+
+        if ((firstNumber > 1) && (firstNumber <= 4) && ((secondNumber < 12) || (secondNumber > 14))) {
+            return "Вам " + age + " года";
+        }
+
+        return "Вам " + age + " лет";
     }
 }
