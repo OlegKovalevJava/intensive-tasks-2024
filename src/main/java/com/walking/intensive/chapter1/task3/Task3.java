ecd59c90c61a122af151e370938ad8c276aee2b1
Oleg <kovalyovop@yandex.ru>
2024-11-19 21:44:04 +0300

    task3 19/11/2024 На самом деле if добавил так как думал, что это лучше для читаемости. Сделал все в одном return. Получается если есть возможность то лучше так сокращать?

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -42,10 +42,6 @@ public class Task3 {
             return false;
         }
 
-        if ((year % 4 == 0) && (year % 100 != 0)) {
-            return true;
-        }
-
-        return year % 400 == 0;
+        return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
     }
 }
