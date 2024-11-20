5d862023c5690f789cda5acc5421c15e4dfe1029
Oleg <kovalyovop@yandex.ru>
2024-11-20 22:18:32 +0300

    task4 20/11/2024

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -24,18 +24,51 @@ package com.walking.intensive.chapter1.task4;
  */
 public class Task4 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+
         double a = 0;
         double b = 0;
         double c = 0;
 
         System.out.println(solveEquation(a, b, c));
-
+        System.out.println(solveEquation(-1, 3, 4));
     }
 
     static String solveEquation(double a, double b, double c) {
-        //        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+        if (a == 0 && b == 0 && c == 0) {
+            return "Бесконечное множество решений.";
+        }
+
+        if (a == 0 && b == 0) {
+            return "Количество решений: 0.";
+        }
+
+        if (a == 0) {
+            int x1 = (int) (-c / b);
+            return "Количество решений: 1. Корень: " + x1;
+        }
+
+        double discriminant = b * b - 4 * a * c;
+
+        return getResult(a, b, discriminant);
+
+    }
+
+    private static String getResult(double a, double b, double discriminant) {
+
+        String result;
+
+        if (discriminant < 0) {
+            result = "Количество решений: 0.";
+        } else if (discriminant == 0) {
+            int x1 = (int) (-b / (2 * a));
+            result = "Количество решений: 1. Корень: " + x1;
+        } else {
+            int x1 = (int) ((-b + Math.sqrt(discriminant)) / (2 * a));
+            int x2 = (int) ((-b - Math.sqrt(discriminant)) / (2 * a));
+            result = "Количество решений: 2. Корни: " + Math.min(x1, x2) + ";" + Math.max(x1, x2);
+        }
+
+        return result;
     }
 }
